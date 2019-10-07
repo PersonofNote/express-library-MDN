9a5cdaffa3ec56efbdc6f6845f3f4fa8749c9eff
Personofnote <apersonofnote@gmail.com>
2019-10-07 11:11:59 -0700

    Complete /users/cool minichallenge

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET challenge 'you're so cool' listing. */
+router.get('/cool', function(req, res, next) {
+  res.send("You're so cool.");
+});
+
 module.exports = router;
