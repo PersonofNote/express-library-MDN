32433eb21d4720b533741ca947bc55123ab03d30
jessica-oro <jessica@oro.co>
2019-11-26 14:15:58 -0800

    Install and run eslint

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.send('respond with a resource');
 });
 
 /* GET challenge 'you're so cool' listing. */
-router.get('/cool', function(req, res, next) {
+router.get('/cool', (req, res) => {
   res.send("You're so cool.");
 });
 
